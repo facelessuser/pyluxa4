c04e9e7f0b16266f832bb324e84bef614149351f
facelessuser <faceless.shop@gmail.com>
2019-09-13 23:25:19 -0600

    Tweak colors

--- pyluxa4/usb.py
+++ pyluxa4/usb.py
@@ -17,12 +17,12 @@ LUXAFOR_PRODUCT = 0xf372
 
 COLOR_MAP = {
     "red": (255, 0, 0),
-    "orange": (255, 102, 0),
+    "orange": (255, 69, 0),
     "yellow": (255, 255, 0),
     "green": (0, 255, 0),
     "blue": (0, 0, 255),
     "magenta": (255, 0, 255),
-    "purple": (75, 0, 130),
+    "indigo": (75, 0, 130),
     "cyan": (0, 255, 255),
     "white": (255, 255, 255),
     "off": (0, 0, 0)
