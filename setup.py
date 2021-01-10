a07fe7151457b39e18e294710265ef80b7c5edb4
facelessuser <faceless.shop@gmail.com>
2021-01-10 10:35:36 -0700

    Update setup file

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
     name='pyluxa4',
     version=VER,
     keywords='luxafor',
+    python_requires=">=3.6",
     description='Luxafor flag controll library/tool.',
     long_description=get_description(),
     long_description_content_type='text/markdown',
@@ -70,9 +71,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
